07457463b3982f0d3d251bcf31a6f6afac1ef0e8
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-13 15:53:39 -0700

    added routes for /scrabble/chart and /scrabble/score/:word

--- routes/scrabble.js
+++ routes/scrabble.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET chart page. */
-router.get('/scrabble/chart', function(req, res, next) {
+router.get('/chart', function(req, res, next) {
   res.render('chart', { title: 'Scrabble Express Chart' });
 });
 
-/* GET chart page. */
-router.get('/scrabble/chart', function(req, res, next) {
-  res.render('chart', { title: 'Scrabble Express Chart' });
+/* GET word score page. */
+router.get('/score/:word', function(req, res, next) {
+  locals.title = 'Scrabble Express Word Score';
+  locals.word = request.params.word;
+  res.render('score', locals);
 });
 
 module.exports = router;
