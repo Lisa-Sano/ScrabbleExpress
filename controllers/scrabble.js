c9a2342da448502205b3cf395720d03b2da95797
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-13 16:23:33 -0700

    added word score to controller and view for /scrabble/score/word

--- controllers/scrabble.js
+++ controllers/scrabble.js
@@ -4,14 +4,16 @@ var scrabble = new Scrabble();
 var ScrabbleController = {
   chart: function(req, res, next) {
     var locals = {};
-    locals.title = 'Scrabble Express Chart'
-    locals.chart = scrabble.letter_scores
+    locals.title = 'Scrabble Express Chart';
+    locals.chart = scrabble.letter_scores;
     res.render('chart', locals);
   },
   score: function(req, res, next) {
     var locals = {};
+    var word = req.params.word
     locals.title = 'Scrabble Express Word Score';
-    locals.word = req.params.word;
+    locals.word = word;
+    locals.score = scrabble.score(word);
     res.render('score', locals);
   }
 }
