b4c8b4f4799e12a918c05d65622fe94e21bd5383
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-13 15:39:01 -0700

    added scrabbleRoutes

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
+var scrabbleRoutes = require('./routes/scrabble');
+app.use('/scrabble', scrabbleRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
